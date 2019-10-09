7e3eee87d385d2f028e363e09c8678f599c7c4bb
atsushi-omachi <pikmin0113>
2019-10-09 22:25:38 +0900

    initialized

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RemindMeAnything
 gradle+angularの勉強用
+
+
+reminderを作るよ
+
+postgresqlを使うよ
+
+## その他やりたいことメモ
+- docker
+- react
+- vue
+- nuxt
+- micro service
+- selenium
+- ci(自動テスト)
---
+++ remindmeanything-api/src/main/java/com/atsushiomachi/remindmeanything/RemindmeanythingApplication.java
@@ -0,0 +1,13 @@
+package com.atsushiomachi.remindmeanything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindmeanythingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemindmeanythingApplication.class, args);
+    }
+
+}
